5bfd75ee80242ee4972f1a5d812c6931f9bb530b
gitvishal96 <95339425+gitvishal96@users.noreply.github.com>
2022-02-08 15:30:04 +0530

    adding main data resource

---
+++ shark.js
@@ -0,0 +1,4 @@
+sharkArray=["9RQX1Ssu3QM", "qX2P0DNFiiM","WXuoYzyCAfw","8tR8LAsjJHY","7Xyr1vT7xnc","7rUAVd-AUfE","wh928LKHSGY",
+"BdVf8ek42pg","cn7mk9M9-pU","LAKi48YSWqU","8tfPOoX3pds","-1_s7kxfmEs","wC6yKdKvNZI","XWRTegHAf08","Yl1KlO__aiE","NKWsbnfIUzc",
+"0kEUs8nE_AE","gq8LRFO7aJQ","64PHoRmrO2c","Ev7rzGsQzBE","CY4dyzihfRw","W7v5OEtC6Zo","Ua3aecjHmMw"];
+
